47aea81e506c72566c7b411d641e8086b5fd5ad7
Chan <chandradhargowtham93@gmail.com>
2019-04-16 17:51:29 +0530

    StackCups 90% implementation only
    Final println not working yet

--- Kattis/StackingCups.java
+++ Kattis/StackingCups.java
@@ -2,8 +2,42 @@ import java.util.*;
 
 class StackingCups
 {
-	public static void main(String[] args) 
+	
+	public static void main(String[] args) throws Exception
 	{
-			
+		HashMap<Integer,String> hm= new HashMap<>();
+		ArrayList<String> al= new ArrayList<>();
+
+		String p1="";
+		String p2="";
+		boolean half=false;
+		Scanner	sc = new Scanner(System.in);
+		int cases=sc.nextInt();
+		while(cases>0)
+		{
+			p1 = sc.next();
+			p2 = sc.next();
+			try
+			{
+				int r=Integer.parseInt(p1);
+				String x=p2;
+				hm.put(r/2,x);
+				al.add(x);
+			}
+			catch(Exception e)
+			{
+				int r= Integer.parseInt(p2);
+				String x = p1;
+				hm.put(r,x);
+				al.add(x);
+			}
+			cases--;
+		}
+		Map<Integer,String> sortedMap = new TreeMap<Integer,String>(hm);
+		    for (int i = 0; i < al.size(); i++)
+				System.out.println(sortedMap.get(al.get(i)));
+
+		//System.out.println(al);
+
 	}
 }
