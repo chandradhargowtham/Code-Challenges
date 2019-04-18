fc9d9740fd836ce35b9e9b66a0b8fd2fd38e762e
Chan <chandradhargowtham93@gmail.com>
2019-04-18 14:10:44 +0530

    Update aaah.java

--- Kattis/aaah.java
+++ Kattis/aaah.java
@@ -4,6 +4,6 @@ class aaah
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);	
-		System.out.println((sc.next().length()>=sc.next().length())?"go":"no");
+		System.out.println((sc.next().length()>=sc.next().length())?"go":"no"); 
 	}
 }
