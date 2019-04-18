acb4a0c6255e62ec294fb9964b9bb7dedb66e7e8
Chan <chandradhargowtham93@gmail.com>
2019-04-18 10:20:46 +0530

    Working files added

--- Kattis/IveBeenEveryWhere.java
+++ Kattis/IveBeenEveryWhere.java
@@ -2,27 +2,34 @@ import java.util.*;
 
 class IveBeenEveryWhere
 {
-	int n=0;
-	int cities=0;
+	static int n=0;
+	static int cities=0;
 
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		cities=sc.nextInt();
-		String visited[]=new String[cities];
-
+		n=sc.nextInt();
+		ArrayList<String> visited=new ArrayList<String>();
 
 		while(n>0)	
 		{
 
-			while(cities>0)
+			visited.clear();
+			cities=sc.nextInt();
+			while(cities>=0)
 			{
 				String s=sc.nextLine();
-				for(int i=0;i<cities;i++)
-				if(!(visited[i].equals(s)))
-
+				if (!visited.contains(s)) 
+				visited.add(s);		
+				cities--;
 			}
-			n--;
+			
+			
+					
+			n--;		
+			visited.remove(0);
+			System.out.println(visited.size());
 		}
+		
 	}
 }
